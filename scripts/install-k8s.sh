cd88475f9e02f806249e20cc4164b213deaeba0d
falenn <dev@imwiz.com>
2021-04-13 20:52:23 +0000

    added notes

--- scripts/install-k8s.sh
+++ scripts/install-k8s.sh
@@ -8,7 +8,7 @@ trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
 trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
-# kubernetes
+# kubernetes network config
 cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
@@ -27,6 +27,9 @@ gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cl
 exclude=kubelet kubeadm kubectl
 EOF
 
+# dir prep
+sudo mkdir -p /etc/cni/net.d
+
 # Set SELinux in permissive mode (effectively disabling it)
 sudo setenforce 0
 sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
@@ -35,3 +38,18 @@ sudo yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
 
 sudo systemctl enable --now kubelet
 #sudo kubeadm config images pull
+
+# deal with some bugs
+#sudo mkdir -p /usr/libexec/kubernetes/kubelet-plugins/volume/exec/nodeagent~uds
+
+# Calicoctl
+#curl -O -L  https://github.com/projectcalico/calicoctl/releases/download/v3.18.1/calicoctl
+#sudo mv calicoctl /usr/local/sbin/calicoctl
+
+# set the env for calicoctl
+#DATASTORE_TYPE=kubernetes KUBECONFIG=~/.kube/config 
+
+# get nodes
+#calicoctl get nodes
+
+
